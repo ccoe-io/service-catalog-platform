107adbc6d105e371836ca02d7598d781279c1516
Ronen Dancziger <ronend4@gmail.com>
2023-10-16 09:14:12 -0400

    fix: missing capability when deploy SAM template dependancy

--- idp/assets/functions/parse/assets/deploy.sh
+++ idp/assets/functions/parse/assets/deploy.sh
@@ -7,6 +7,6 @@ res=$(aws --profile $profile cloudformation deploy --template-file $template_fil
 if [[ $? != 0 ]]; then
     if [[ $res == *"is in ROLLBACK_FAILED state and can not be updated"* ]]; then
         aws --profile $profile cloudformation delete-stack --stack-name $stack_name
-        aws --profile $profile cloudformation deploy --template-file $template_file_name --stack-name $stack_name --no-fail-on-empty-changeset --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM
+        aws --profile $profile cloudformation deploy --template-file $template_file_name --stack-name $stack_name --no-fail-on-empty-changeset --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM CAPABILITY_AUTO_EXPAND
     fi
 fi
