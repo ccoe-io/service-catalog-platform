56393e1975f5469b893e97aac2784c7c6ad7ad12
Ronen Dancziger <ronend4@gmail.com>
2023-10-16 08:53:48 -0400

    fix: secret key is echoed in build project

--- idp/assets/functions/parse/assets/session.sh
+++ idp/assets/functions/parse/assets/session.sh
@@ -4,10 +4,10 @@ role_arn=$1
 OUTPUT_PROFILE=$2
 
 echo $OUTPUT_PROFILE
-echo $role_arn
+echo Assume Role: $role_arn
 sts=$(aws sts assume-role --role-arn $role_arn --role-session-name $OUTPUT_PROFILE --query 'Credentials.[AccessKeyId,SecretAccessKey,SessionToken]' --output text)
-echo 'Converting sts to array'
-echo $sts
+# echo 'Converting sts to array'
+# echo $sts
 # IFS=', ' read -r -a sts <<< $sts
 sts=($sts)
 echo AWS_ACCESS_KEY_ID is ${sts[0]}
